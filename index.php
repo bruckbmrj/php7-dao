05160a22cae782b9fa66c1cf0d0d7679f4e91af4
bruckbmrj <bruckbmrj@hotmail.com>
2018-01-16 18:50:28 -0200

    inserir e alterar

--- index.php
+++ index.php
@@ -16,8 +16,24 @@ require_once ("config.php");
 //echo json_encode($search);
 
 //Carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("root","!@#$");
+
+//echo $usuario;
+
+/*Criando um novo usuario (insert)
+$aluno = new Usuario("aluno","@lun0");
+
+$aluno->insert();
+
+echo $aluno;
+*/
+
 $usuario = new Usuario();
-$usuario->login("root","!@#$");
+
+$usuario->loadbyId(7);
+
+$usuario->update("professor", "!@#$%&");
 
 echo $usuario;
 
